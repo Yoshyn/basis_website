c46cab8aa5039934a36b09d81e46b8e316e5f9d9
sylvestre-laptop <sylvestre@ciblo.net>
2013-11-22 00:50:00 +0100

    Fix on nilClass && add rbenv version

--- lib/tiny_rafine/nil_class.rb
+++ lib/tiny_rafine/nil_class.rb
@@ -7,12 +7,9 @@ module TinyRafine
       end
 
       # Define a redirection from implicit to explicit conversion for Integer, String, Array and Hash
-      ['int', 'str', 'ary', 'hash'].each do |method| #FIXMEFAIL
-        define_method "to_#{method[0]}" do
-          Rails.logger.error "send to_#{method}"
-          res = send "to_#{method}"
-          Rails.logger.error "res : #{res}"
-          res
+      ['int', 'str', 'hash'].each do |method| #FIXMEFAIL 'ary'
+        define_method "to_#{method}" do
+          send "to_#{method[0]}"
         end
       end
     end
